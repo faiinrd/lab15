c4c65b7e97d6107a43c769adcbd684803516a15a
faiinrd <nopradakrit@gmail.com>
2024-01-26 16:40:03 +0700

    lab15 completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,14 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int A[] = {a,b,c,d};
+	for(int i=0; i<4; i++){
+		int j = rand()%4;
+		int x=A[i];
+		A[i]=A[j];
+		A[j]=x;
+	}
+	a=A[0]; b=A[1]; c=A[2]; d=A[3];
+}
